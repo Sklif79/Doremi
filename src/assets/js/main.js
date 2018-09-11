2b5cc9c4b59691297beeb803afeabcb4e9c8f1fa
K. Liutskevich <k.liutskevich@gmail.com>
2018-09-11 21:31:44 +0300

    панель поиска

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+    getWindowWidth();
+
     //sliders
-    productSliderInit();
+    //productSliderInit();
 
     $('.js_articles-slider').slick({
         slidesToShow: 3,
@@ -14,7 +16,15 @@ $(document).ready(function () {
         slidesToScroll: 1,
         infinite: true,
         arrows: true,
-        rows: 2
+        rows: 2,
+        responsive: [
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 6,
+                }
+            }
+        ]
     });
 
     //custom select
@@ -44,38 +54,68 @@ $(document).ready(function () {
     productSliderHeight();
     miniCard();
     toUp();
-    //searchResultCornerLeftPosition();
+    searchResultCornerLeftPosition();
+    headerBasketMove();
+    searchMobile();
 });
 
 $(window).resize(function () {
+    clearSearchField();
     productSliderHeight();
-    //searchResultCornerLeftPosition();
+    searchResultCornerLeftPosition();
+    getWindowWidth();
+    headerBasketMove();
+    searchMobile();
 });
 
 function clearSearchField() {
     var $resetBtn = $('.search__reset'),
         $searchResult = $('.search-result');
 
-    $('.search__field').on('keyup', function () {
-        if ($(this).val()) {
-            $resetBtn.css({"visibility": "visible"});
+    $('.search__field').off('keyup');
+    $resetBtn.off('click');
+    $searchResult.removeClass('active');
 
-            //сюда можно впихать аякс для поиска,
-            //если нужен поиск после каждой введенной буквы (добавить переинициализацию слайдера и высоты продуктов)
-            //productSliderHeight();
-            //productSliderInit();
-            $searchResult.addClass('active');
+    if (getWindowWidth() > 1200) {
+        $('.search__field').on('keyup', function () {
+            if ($(this).val()) {
+                $resetBtn.css({"visibility": "visible"});
+                $searchResult.addClass('active');
 
-        } else {
-            $resetBtn.css({"visibility": "hidden"});
+                //сюда можно впихать аякс для поиска,
+                //если нужен поиск после каждой введенной буквы (добавить переинициализацию слайдера и высоты продуктов)
+                //productSliderHeight();
+                //productSliderInit();
+
+
+            } else {
+                $resetBtn.css({"visibility": "hidden"});
+                $searchResult.removeClass('active');
+            }
+        });
+
+        $resetBtn.on("click", function () {
+            $(this).css({"visibility": "hidden"});
             $searchResult.removeClass('active');
-        }
-    });
+        });
+    }
+}
 
-    $resetBtn.on("click", function () {
-        $(this).css({"visibility": "hidden"});
-        $searchResult.removeClass('active');
-    });
+function searchMobile() {
+    var $searchMobileBtn = $('.header-bottom-search-btn');
+
+    $searchMobileBtn.off('click').removeClass('active');
+    $('.header-nav, .search').css({'display': ''});
+    $('.header-bottom-right').removeClass('active');
+
+    if (getWindowWidth() <= 1200) {
+        $searchMobileBtn.on('click', function () {
+            $(this).toggleClass('active');
+            $('.search').toggle();
+            $('.header-nav').toggle();
+            $('.header-bottom-right').toggleClass('active');
+        });
+    }
 }
 
 function searchResultCornerLeftPosition() {
@@ -89,15 +129,16 @@ function productSliderHeight() {
 
 //миникарта активируется, когда внутри есть элементы
 function miniCard() {
-    var leaveBasket = false;
+    var leaveBasket = false,
+        $body = $('body');
 
     if ($('.header-basket-minicard__item').length) {
-        $('body').on('mouseenter', '.header-basket, .header-basket-minicard', function () {
+        $body.on('mouseenter', '.header-basket, .header-basket-minicard', function () {
             leaveBasket = false;
             $('.header-basket-minicard').addClass('active');
         });
 
-        $('body').on('mouseleave', '.header-basket, .header-basket-minicard', function () {
+        $body.on('mouseleave', '.header-basket, .header-basket-minicard', function () {
             leaveBasket = true;
             checkLeaveBasket();
         });
@@ -131,8 +172,30 @@ function productSliderInit() {
         slidesToShow: 6,
         slidesToScroll: 1,
         infinite: true,
-        arrows: true
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 5,
+                }
+            }
+        ]
     });
+}
+
+function headerBasketMove() {
+    var $basketCount = $('.header-basket__count');
+
+    if (getWindowWidth() <= 1200) {
+        $('.header-basket').append($basketCount);
+    } else {
+        $('.header-basket__desktop-subtitle').prepend($basketCount);
+    }
+}
+
+function getWindowWidth() {
+    return window.mainWidth = $(window).width();
 }
 
 $.fn.setMaxHeights = function () {
